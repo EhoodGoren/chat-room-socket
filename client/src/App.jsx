c5b198fa60a0150d9907c7b58ef66ab13a4e79e0
EhoodGoren <ehoodgoren@gmail.com>
2021-12-31 00:47:17 +0200

    Added message sending with user name, and display of all messages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,31 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import io from 'socket.io-client';
 
 function App() {
+    const [messages, setMessages] = useState([]);
     const socketRef = useRef();
+
     useEffect(() => {
-        socketRef.current = io.connect('http://localhost:8080/');
+        socketRef.current = io.connect('http://localhost:8080', {
+            query: {
+                user: 'someone'
+            }
+        });
         
-        socketRef.current.on('messageBack', (data) => {
-            console.log(data);
+        socketRef.current.on('messageBack', (message) => {
+            setMessages(msgs => [...msgs, message])
         })
     }, [])
+
     const sendMessage = () => {
-        socketRef.current.emit('message', 'hello');
+        socketRef.current.emit('message', {name: 'someone', message: 'hello'});
     }
+
     return (
         <div>
+            {messages.map(({ name, message }, index) => (
+                <div key={`${message}${index}`}>{name}: {message}</div>
+            ))}
             <button onClick={sendMessage}>Send</button>
         </div>
     )
