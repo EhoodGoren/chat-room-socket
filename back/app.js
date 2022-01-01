5516dce6322a5eb7bfbda4b1073b58ae15ccab2c
EhoodGoren <ehoodgoren@gmail.com>
2022-01-02 01:37:03 +0200

    Fixed client path

--- back/app.js
+++ back/app.js
@@ -14,7 +14,7 @@ const io = socketIo(server, {
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 let onlineUsers = [];
 io.on('connection', (socket) => {
